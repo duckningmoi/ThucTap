d45e27f3a86a855637354b025d5bc224980692c1
Minhphuong1210 <chuminhphuong100@gmail.com>
2024-09-15 10:42:51 +0700

    testapi

--- app/Http/Controllers/Api/ApiPostController.php
+++ app/Http/Controllers/Api/ApiPostController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use DB;
 use Illuminate\Http\Request;
 
 class ApiPostController extends Controller
 {
-    Public function index(){
-        
+    Public function Trangchu(){
+        $post=DB::table('posts')->where('is_approved',1)->get();
+        return response()->json([
+            'post'=>$post
+        ],200);
     }
 }
