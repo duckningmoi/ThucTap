4edb3fba4474d605e7e6a16ab4391e39936c7ca4
Minhphuong1210 <chuminhphuong100@gmail.com>
2024-10-11 22:06:49 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/',[ApiPostController::class,'Trangchu'])->name('trangchu');
-Route::get('/header',[ApiPostController::class,'category'])->name('abc');
+Route::get('/category',[ApiPostController::class,'category'])->name('abc');
 Route::get('/post/{id_category}',[ApiPostController::class,'filterPost'])->name('abc');
+Route::get('/postDetail/{slug}',[ApiPostController::class,'PostDetail']);
 
